fd0a7e73094e13cfbb4a87425cbc626d0bd51168
Kheng-Bee Goh <WalterKPhillips@gmail.com>
2016-05-17 12:11:44 -0400

    Super simple assignment.

--- pythonInformatics/src/assign3_3.py
+++ pythonInformatics/src/assign3_3.py
@@ -1,1 +1,19 @@
-score = raw_input("Enter Score: ")
+score = float(raw_input("Enter Score: "))
+grade = None
+
+if score >= 0.9:
+    grade = 'A'
+elif score < 0.9 and score >= 0.8:
+    grade = 'B'
+elif score < 0.8 and score >= 0.7:
+    grade = 'C'
+elif score < 0.7 and score >= 0.6:
+    grade = 'D'
+else:
+    grade = 'F'
+
+print grade
+
+
+
+
